53477acbfda59d7888cbf8b6ad4c978ff059e16a
DIVYAN TM <123184476+DivyanTM@users.noreply.github.com>
2024-05-31 15:59:04 +0530

    main method in equation

--- src/Equation.java
+++ src/Equation.java
@@ -11,6 +11,15 @@ public class Equation {
         rhs=(int)Math.pow(a,3)+(3*(int)Math.pow(a,2)*b)+(3*a*(int)Math.pow(b,2))+(int)Math.pow(b,3);
         return lhs==rhs;
     }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two numbers : ");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        Equation eq=new Equation(a,b);
+        System.out.print("lhs : "+eq.lhs+"\nrhs : "+eq.rhs);
+        eq.prove()?System.out.println("equation proved"):System.out.println("equation not proved");
+    }
 }
 
 
